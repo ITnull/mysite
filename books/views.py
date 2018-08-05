0c68ced5d408128302c9517278918487d37dbf75
ITnull <1209016337@qq.com>
2018-08-05 11:38:28 +0800

    修改表单

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -11,12 +12,15 @@ def search_form(request):
 
 
 def search(request):
-    error = False
+    #error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-	    error = True
+	    errors.append('输入待搜索的内容.')
+ 	elif len(q)>20:
+	    errors.append('请输入小于20个字符的内容.')
   	else:
 	    books = Book.objects.filter(title__icontains=q)
             return render_to_response('search_results.html',{'books': books, 'query': q})
-    return render_to_response('search_form.html',{'error': error})
+    return render_to_response('search_form.html',{'errors': errors})
